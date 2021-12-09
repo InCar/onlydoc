41ebe7895cdec56b4e616c18346b8222c7025ba0
qlhan <qlhan@incarcloud.com>
2021-12-09 17:53:49 +0800

    1.0.2 onlyoffice integration

--- README.md
+++ README.md
@@ -1,1 +1,222 @@
 # onlydoc
+
+[![java](https://img.shields.io/badge/java-1.8-brightgreen.svg?style=flat&logo=java)](https://www.oracle.com/java/technologies/javase-downloads.html)
+[![spring](https://img.shields.io/badge/springboot-2.3.2-brightgreen.svg?style=flat&logo=spring)](https://docs.spring.io/spring-boot/docs/2.3.x-SNAPSHOT/reference/htmlsingle)
+[![gradle](https://img.shields.io/badge/gradle-7.2-brightgreen.svg?style=flat&logo=gradle)](https://docs.gradle.org/7.2/userguide/installation.html)
+[![build](https://github.com/InCar/onlydoc/workflows/build/badge.svg)](https://github.com/InCar/onlydoc/actions)
+[![release](https://img.shields.io/badge/release-1.0.2-blue.svg)](https://github.com/InCar/onlydoc/releases)
+
+> OnlyOffice Integration.
+
+## 1 Development
+
+> [Java with Spring Boot](https://api.onlyoffice.com/editors/example/javaspring)
+
+#### 1.1 application-dev.yml
+
+```yaml
+# Files settings
+files:
+  docservice:
+    url:
+      site: http://127.0.0.1:18080/onlyoffice/
+
+# Logo settings
+logo:
+  url: https://www.incarcloud.com
+```
+
+#### 1.2 Build
+
+```bash
+./gradlew clean build
+```
+
+## 2. Docker image for `onlydoc:1.0.2`
+
+### 2.1 Dockerfile
+
+```dockerfile
+# java-backend with oracle-jdk8(alpine)
+# Author: InCar
+# Version: 1.0
+# Build:
+#   1. docker build --build-arg APP_NAME=java-backed \
+#        --build-arg APP_VERSION=1.0 \
+#        -t s1:5000/harbor_project/java-backed:1.0 .
+# Testing:
+#   1. docker run --rm -it --name backed -p 8080:8080 s1:5000/harbor_project/java-backend:1.0
+#   2. docker exec -it backend ash
+FROM s1:5000/library/oracle-jdk8:latest
+
+MAINTAINER InCar
+
+ARG APP_NAME=java-backed
+ARG APP_VERSION=1.0
+
+ENV APP_NAME=${APP_NAME} \
+  APP_VERSION=${APP_VERSION}
+
+EXPOSE 4000
+
+WORKDIR /app
+
+ADD build/libs/${APP_NAME}-${APP_VERSION}.jar /app/deploy.jar
+
+ENTRYPOINT ["java", "-jar", "deploy.jar", "-Dsun.net.http.allowRestrictedHeaders=true"]
+```
+
+### 2.2 Build Image
+
+```bash
+docker build --build-arg APP_NAME=onlydoc \
+  --build-arg APP_VERSION=1.0.2 \
+  -t onlydoc:1.0.2 .
+```
+
+## 3. Deployment
+
+> [Overview of Docker Compose](https://docs.docker.com/compose/)
+
+|No.|Service|Inter Port|Outer Port|Remark|
+|:---:|:---:|:---:|:---:|----|
+|1|*onlyoffice*|`80`|`8080`|*will proxy by nginx*|
+|2|*onlydoc*|`4000`|`4000`||
+
+```bash
+cat > docker-compose.yaml <<-'EOF'
+version: "3"
+services:
+  onlyoffice:
+    image: onlyoffice/documentserver:6.4.2
+    container_name: onlyoffice_community_edition
+#    volumes:
+#      - ./onlyoffice/logs:/var/log/onlyoffice
+#      - ./onlyoffice/data:/var/www/onlyoffice/Data
+#      - ./onlyoffice/lib:/var/lib/onlyoffice
+#      - ./onlyoffice/db:/var/lib/postgresql
+    dns:
+      - 8.8.8.8
+    environment:
+      - ONLYOFFICE_HTTPS_HSTS_ENABLED=false
+    ports:
+      - 8080:80
+    restart: always
+    deploy:
+      restart_policy:
+        condition: on-failure
+  onlydoc:
+    image: onlydoc:1.0.2
+    container_name: onlydoc_integration_edition
+    environment:
+      - DOCUMENTSERVER_SITE=http://127.0.0.1:18080/onlyoffice/
+    ports:
+      - 4000:4000
+    restart: always
+    deploy:
+      restart_policy:
+        condition: on-failure
+EOF
+```
+
+## 4 Nginx
+
+### 4.1 nginx.conf
+
+```nginx
+map $http_upgrade $connection_upgrade {
+    default upgrade;
+    '' close;
+}
+
+upstream onlydoc-servers {
+    server 127.0.0.1:4000;
+}
+
+upstream onlyoffice-servers {
+    server 127.0.0.1:8080;
+}
+
+server {
+    listen       18080;
+    server_name localhost;
+
+    charset utf-8;
+
+    # onlydoc
+    location ^~ /onlydoc/ {
+        proxy_pass http://onlydoc-servers/;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $http_host/onlydoc;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Real-PORT $remote_port;
+    }
+
+    # onlyoffice
+    location ^~ /onlyoffice/ {
+        proxy_pass http://onlyoffice-servers/;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $http_host/onlyoffice;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Real-PORT $remote_port;
+        proxy_connect_timeout 15s;
+        proxy_read_timeout 120s;
+        proxy_send_timeout 60s;
+    }
+    
+    # add your project proxy here
+}
+```
+
+### 4.2 Reload
+
+```bash
+# valid
+/usr/local/nginx/sbin/nginx -t
+
+# reload
+/usr/local/nginx/sbin/nginx -s reload
+```
+
+## 5 Usage
+
+### 5.1 Params
+
+```text
+http://127.0.0.1:18080/onlydoc/editor?action=view
+  &fileName=test.xlsx
+  &fileKey=test2xlsx
+  &fileUrl=http://10.0.11.25:9333/1,01fc39db1909
+  &thirdUri=true
+  &lang=zh
+  &displayName=张三
+```
+
+|No.|Name|Type|Remark|
+|:---:|:---:|:----|-----|
+|1|action|`string`|*模式：view-预览*|
+|2|fileName|`string`|*文件名称*|
+|3|fileKey|`string`|*文件在OnlyOffice缓存Key，必需唯一，默认同fileName*|
+|4|fileUrl|`string`|*文件外部URL，当`thirdUri=true`时有效*|
+|5|thirdUri|`boolean`|*使用文件外部URL*|
+|6|lang|`string`|*语言：en-英文（默认），zh-中文*|
+|7|displayName|`string`|*使用人，没有显示“匿名”*|
+
+### 5.2 Testing
+
+|No.|File|Testing|Remark|
+|:---:|:---:|:---:|-----|
+|1|[test.xlsx](http://10.0.11.25:9333/1,01fc39db1909)|[Excel](http://127.0.0.1:18080/onlydoc/editor?action=view&fileName=test.xlsx&fileKey=test2xlsx&fileUrl=http://10.0.11.25:9333/1,01fc39db1909&thirdUri=true&lang=zh&displayName=张三)|*InCar Only*|
+|2|[test.docx](http://10.0.11.25:9333/6,01fe0f8fd5a4)|[Word](http://127.0.0.1:18080/onlydoc/editor?action=view&fileName=test.docx&fileKey=test2docx&fileUrl=http://10.0.11.25:9333/6,01fe0f8fd5a4&thirdUri=true&lang=zh&displayName=张三)||
+|3|[test.pptx](http://10.0.11.25:9333/6,01ff978bc1c3)|[PowerPoint](http://127.0.0.1:18080/onlydoc/editor?action=view&fileName=test.pptx&fileKey=test2pptx&fileUrl=http://10.0.11.25:9333/6,01ff978bc1c3&thirdUri=true&lang=zh&displayName=张三)||
+
+### 6 About File Access Limit
+
+&emsp;&emsp;**It is recommended to separate view and access, adding your token to `fileUrl`.**
---
+++ src/main/java/com/onlyoffice/integration/controllers/EditorController.java
@@ -0,0 +1,224 @@
+/**
+ * (c) Copyright Ascensio System SIA 2021
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.onlyoffice.integration.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onlyoffice.integration.documentserver.managers.history.HistoryManager;
+import com.onlyoffice.integration.documentserver.managers.jwt.JwtManager;
+import com.onlyoffice.integration.documentserver.models.enums.Action;
+import com.onlyoffice.integration.documentserver.models.enums.Language;
+import com.onlyoffice.integration.documentserver.models.enums.Type;
+import com.onlyoffice.integration.documentserver.models.filemodel.Document;
+import com.onlyoffice.integration.documentserver.models.filemodel.EditorConfig;
+import com.onlyoffice.integration.documentserver.models.filemodel.FileModel;
+import com.onlyoffice.integration.documentserver.storage.FileStoragePathBuilder;
+import com.onlyoffice.integration.documentserver.util.Misc;
+import com.onlyoffice.integration.dto.Mentions;
+import com.onlyoffice.integration.entities.User;
+import com.onlyoffice.integration.services.UserServices;
+import com.onlyoffice.integration.services.configurers.FileConfigurer;
+import com.onlyoffice.integration.services.configurers.wrappers.DefaultFileWrapper;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@CrossOrigin("*")
+@Controller
+public class EditorController {
+
+    private Logger logger = LoggerFactory.getLogger(EditorController.class);
+
+    @Value("${files.docservice.url.site}")
+    private String docserviceSite;
+
+    @Value("${files.docservice.url.api}")
+    private String docserviceApiUrl;
+
+    @Autowired
+    private FileStoragePathBuilder storagePathBuilder;
+
+    @Autowired
+    private JwtManager jwtManager;
+
+    @Autowired
+    private UserServices userService;
+
+    @Autowired
+    private HistoryManager historyManager;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private FileConfigurer<DefaultFileWrapper> fileConfigurer;
+
+    @GetMapping(path = "${url.editor}")
+    public String index(@RequestParam("fileName") String fileName,
+                        @RequestParam(value = "action", required = false) String actionParam,
+                        @RequestParam(value = "type", required = false) String typeParam,
+                        @RequestParam(value = "actionLink", required = false) String actionLink,
+                        @RequestParam(value = "fileKey", required = false) String fileKey,
+                        @RequestParam(value = "fileUrl", required = false) String fileUrl,
+                        @RequestParam(value = "thirdUri", required = false) Boolean thirdUri,
+                        @RequestParam(value = "lang", required = false) String lang,
+                        @RequestParam(value = "displayName", required = false) String displayName,
+                        @CookieValue(value = "uid", required = false) String uid,
+                        @CookieValue(value = "ulang", required = false) String ulang,
+                        Model model) throws JsonProcessingException {
+        Action action = Action.edit;
+        Type type = Type.desktop;
+        Language language = Language.en;
+
+        if (actionParam != null) {
+            action = Action.valueOf(actionParam);
+        }
+        if (typeParam != null) {
+            type = Type.valueOf(typeParam);
+        }
+        if (lang != null) {
+            language = Language.valueOf(lang);
+        } else if (ulang != null) {
+            language = Language.valueOf(ulang);
+        }
+
+        // Third UID
+        // Default John Smith (uid=1).
+        // com.onlyoffice.integration.documentserver.managers.history.DefaultHistoryManager#L84
+        if (StringUtils.isBlank(uid)) {
+            uid = "1";
+        }
+
+        Optional<User> optionalUser = userService.findUserById(Integer.parseInt(uid));
+
+        if (!optionalUser.isPresent()) {
+            return "index.html";
+        }
+
+        User user = optionalUser.get();
+
+        FileModel fileModel = fileConfigurer.getFileModel(
+                DefaultFileWrapper
+                        .builder()
+                        .fileName(fileName)
+                        .type(type)
+                        .lang(language)
+                        .action(action)
+                        .user(user)
+                        .actionData(actionLink)
+                        .build()
+        );
+
+        // Third URI
+        if (null != thirdUri && thirdUri) {
+            Document document = fileModel.getDocument();
+            if (StringUtils.isNotBlank(fileKey)) {
+                document.setKey(fileKey);
+            } else {
+                document.setKey(fileName);
+            }
+            document.setUrl(fileUrl);
+            document.getPermissions().setEdit(false);
+            document.getPermissions().setComment(false);
+
+            EditorConfig editorConfig = fileModel.getEditorConfig();
+            if (StringUtils.isBlank(displayName)) {
+                displayName = "匿名";
+            }
+            editorConfig.getUser().setName(displayName);
+            editorConfig.getCustomization().setChat(false);
+            editorConfig.getCustomization().setComments(false);
+
+            logger.info("{} -> {}({}) -> {}", displayName, fileName, document.getKey(), document.getUrl());
+        }
+
+        model.addAttribute("model", fileModel);
+        model.addAttribute("fileHistory", historyManager.getHistory(fileModel.getDocument()));
+        model.addAttribute("docserviceApiUrl", docserviceSite + docserviceApiUrl);
+        model.addAttribute("dataInsertImage", getInsertImage());
+        model.addAttribute("dataCompareFile", getCompareFile());
+        model.addAttribute("dataMailMergeRecipients", getMailMerge());
+        model.addAttribute("usersForMentions", getUserMentions(uid));
+
+        return "editor.html";
+    }
+
+    private List<Mentions> getUserMentions(String uid) {
+        List<Mentions> usersForMentions = new ArrayList<>();
+        if (uid != null && !uid.equals("4")) {
+            List<User> list = userService.findAll();
+            for (User u : list) {
+                if (u.getId() != Integer.parseInt(uid) && u.getId() != 4) {
+                    usersForMentions.add(new Mentions(u.getName(), u.getEmail()));
+                }
+            }
+        }
+
+        return usersForMentions;
+    }
+
+    @SneakyThrows
+    private String getInsertImage() {
+        Map<String, Object> dataInsertImage = new HashMap<>();
+        dataInsertImage.put("fileType", "png");
+        dataInsertImage.put("url", storagePathBuilder.getServerUrl(true) + "/css/img/logo.png");
+
+        if (jwtManager.tokenEnabled()) {
+            dataInsertImage.put("token", jwtManager.createToken(dataInsertImage));
+        }
+
+        return objectMapper.writeValueAsString(dataInsertImage).substring(1, objectMapper.writeValueAsString(dataInsertImage).length() - 1);
+    }
+
+    @SneakyThrows
+    private String getCompareFile() {
+        Map<String, Object> dataCompareFile = new HashMap<>();
+        dataCompareFile.put("fileType", "docx");
+        dataCompareFile.put("url", storagePathBuilder.getServerUrl(true) + "/assets?name=sample.docx");
+
+        if (jwtManager.tokenEnabled()) {
+            dataCompareFile.put("token", jwtManager.createToken(dataCompareFile));
+        }
+
+        return objectMapper.writeValueAsString(dataCompareFile);
+    }
+
+    @SneakyThrows
+    private String getMailMerge() {
+        Map<String, Object> dataMailMergeRecipients = new HashMap<>();
+        dataMailMergeRecipients.put("fileType", "csv");
+        dataMailMergeRecipients.put("url", storagePathBuilder.getServerUrl(true) + "/csv");
+
+        if (jwtManager.tokenEnabled()) {
+            dataMailMergeRecipients.put("token", jwtManager.createToken(dataMailMergeRecipients));
+        }
+
+        return objectMapper.writeValueAsString(dataMailMergeRecipients);
+    }
+}
